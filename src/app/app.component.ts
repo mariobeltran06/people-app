63258796388de65133dd2e5ae1115a3af61e448b
Mario Beltran <mariobeltran0699@gmail.com>
2023-06-11 13:15:41 -0600

    ♻️ refactor code on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { RouterOutlet } from '@angular/router';
+import { LoadingService } from './modules/core/services/loading.service';
 import { ToolbarComponent } from './modules/shared/components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent, MatProgressBarModule],
+  imports: [CommonModule, RouterOutlet, ToolbarComponent, MatProgressBarModule],
+  providers: [LoadingService],
   template: `
     <app-toolbar></app-toolbar>
-    <mat-progress-bar color="accent" mode="indeterminate"></mat-progress-bar>
+    <mat-progress-bar
+      *ngIf="loading$ | async"
+      color="accent"
+      mode="indeterminate"></mat-progress-bar>
     <router-outlet></router-outlet>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  loading$ = this.loader.loading$;
+  constructor(private loader: LoadingService) {}
+}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { HttpErrorInterceptor } from './modules/core/interceptors/http-errors.interceptor';
+import { LoadInterceptor } from './modules/core/interceptors/load.interceptor';
+import { LoadingService } from './modules/core/services/loading.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    LoadingService,
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadInterceptor, multi: true },
+  ],
 };
