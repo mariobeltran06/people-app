fe904858db5b2da8ec3b303049ee4529426646f4
Mario Beltran <mariobeltran0699@gmail.com>
2023-06-11 13:15:20 -0600

    ✨ add loading service

---
+++ src/app/modules/core/services/loading.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable()
+export class LoadingService {
+  private _load = new BehaviorSubject<boolean>(false);
+  readonly loading$ = this._load.asObservable();
+
+  show() {
+    this._load.next(true);
+  }
+
+  hide() {
+    this._load.next(false);
+  }
+}
