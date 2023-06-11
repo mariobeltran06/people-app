8160e0d16bd371c7d5929c63065354b20ad1d339
Mario Beltran <mariobeltran0699@gmail.com>
2023-06-11 01:23:16 -0600

    ⚡️ configure app component and app config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './modules/shared/components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [RouterOutlet, ToolbarComponent, MatProgressBarModule],
   template: `
     <app-toolbar></app-toolbar>
+    <mat-progress-bar color="accent" mode="indeterminate"></mat-progress-bar>
     <router-outlet></router-outlet>
   `,
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,18 @@
 import { Routes } from '@angular/router';
 import { RegisterPersonComponent } from './modules/people/pages/register-person/register-person.component';
 import { RegisteredPersonsComponent } from './modules/people/pages/registered-persons/registered-persons.component';
+import { PersonService } from './modules/people/services/person.service';
 
 export const routes: Routes = [
     {
         path: '',
-        component: RegisterPersonComponent
+        component: RegisterPersonComponent,
+        providers: [PersonService]
     },
     {
         path: 'registered-persons',
-        component: RegisteredPersonsComponent
+        component: RegisteredPersonsComponent,
+        providers: [PersonService]
     },
     {
         path: '**',
