f21f11544b426b96b55b095d9f2e36c2a87ec295
Mario Beltran <mariobeltran0699@gmail.com>
2023-06-11 12:48:37 -0600

    💄 update styles on pages

--- src/app/modules/people/pages/registered-persons/registered-persons.component.ts
+++ src/app/modules/people/pages/registered-persons/registered-persons.component.ts
@@ -1,21 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { ListPeopleComponent } from '../../components/list-people/list-people.component';
+import { NoDataPeopleComponent } from '../../components/no-data-people/no-data-people.component';
+import { Person } from '../../interfaces/person.interface';
 import { ResponsePeople } from '../../interfaces/response-people.interface';
 import { PersonService } from '../../services/person.service';
 
 @Component({
   selector: 'app-registered-persons',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    NoDataPeopleComponent,
+    ListPeopleComponent,
+  ],
   templateUrl: './registered-persons.component.html',
-  styleUrls: ['./registered-persons.component.scss']
+  styleUrls: ['./registered-persons.component.scss'],
 })
 export class RegisteredPersonsComponent {
-  constructor(private personService: PersonService){}
+  dataPeople: Person[] = [];
+  constructor(private personService: PersonService) {}
 
-  ngOnInit(): void{
-    this.personService.getRegistreredPersons().subscribe((data: ResponsePeople) => {
-      console.log(data);
+  ngOnInit(): void {
+    this.personService.getRegistreredPersons().subscribe({
+      next: (data: ResponsePeople) => {
+        this.dataPeople = data.people;
+      },
+      error: () => (this.dataPeople = []),
     });
   }
 }
